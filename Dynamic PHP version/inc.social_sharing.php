3be59db01176d8beee59f1d624ef968f228dfdd9
akhilgopinat <Akhil_Gopinath@condenast.com>
2014-06-01 11:35:57 -0400

    akhiframes php and html version files

---
+++ Dynamic PHP version/inc.social_sharing.php
@@ -0,0 +1,33 @@
+<div id="social_sharing">
+    <ul class="social-connect clearfix">
+        <li class="fb_like_button">
+            <div class="bg_over"></div>
+            <div class="inner">
+                <div class="fb-like" data-href="http://www.hogash.com/demo/ekho_html/" data-send="false" data-layout="button_count" data-width="80" data-show-faces="false" data-font="arial"></div>
+            </div>    
+        </li><!-- end FB Like button  -->
+    
+        <li class="tweetme">
+            <div class="bg_over"></div>
+            <div class="inner">
+                <a href="http://twitter.com/share" class="twitter-share-button" data-text="Check out this great new #html #template" data-via="hogash" data-url="http://hogash.com/demo/ekho_html/" data-count="horizontal" >Tweet</a>
+                <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+            </div>
+        </li><!-- end tweet button -->
+    
+        <li class="gplus">
+            <div class="bg_over"></div>
+            <div class="inner">
+                <script type="text/javascript">
+                (function() {
+                var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+                po.src = 'https://apis.google.com/js/plusone.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+                })();
+                </script>
+                <div class="g-plusone" data-size="medium"></div>
+            </div>
+        </li><!-- end gplus -->
+    </ul>
+    <div class="clear"></div>
+</div>
